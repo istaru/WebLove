4148cf1dfb7d0a12f7b316f523fc7c552eac3e39
朱亮亮 <zhu-l@aeonpeople.biz>
2024-02-19 20:51:46 +0800

    1

--- js/functions.js
+++ js/functions.js
@@ -99,23 +99,26 @@ function startHeartAnimation() {
 })(jQuery);
 function timeElapse(c) {
     var e = Date();
-    var f = (Date.parse(e) - Date.parse(c)) / 1000;
+    var f = (Date.parse(c) - Date.parse(e)) / 1000;
     var g = Math.floor(f / (3600 * 24));
     f = f % (3600 * 24);
     var b = Math.floor(f / 3600);
     if (b < 10) {
         b = "0" + b
     }
+
     f = f % 3600;
     var d = Math.floor(f / 60);
     if (d < 10) {
         d = "0" + d
     }
+
     f = f % 60;
     if (f < 10) {
         f = "0" + f
     }
-    var a = '<span class="digit">' + g + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+
+    var a = '<span class="digit">' + g + '</span> 天 <span class="digit">' + b + '</span> 小时 <span class="digit">' + d + '</span> 分钟 <span class="digit">' + f + "</span> 秒";
     $("#elapseClock").html(a)
 }
 function showMessages() {
